0337bf0c059b8fdb4a3b02dd7ddc77f88d3f5d67
Enki Yan <yansi.cn@gmail.com>
2025-12-05 08:02:16 +0900

    refactor: Remove unused React import from App.tsx
    The `React` import was no longer necessary as it was not being used directly within the `App.tsx` file. This change cleans up the import statement.

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import RegionShowcase from './components/RegionShowcase';
